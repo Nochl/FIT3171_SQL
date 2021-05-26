5d98cd6ae8eaae8a5d3739b40f69fa0503571448
eleo0010 <eleo0010@MVAZ2STUL01003.ad.monash.edu>
2021-05-26 23:52:44 +1000

    Completed Task 1 Q2-5

--- Assignments/Ass2B/mh-queries.sql
+++ Assignments/Ass2B/mh-queries.sql
@@ -18,7 +18,7 @@
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
-SELECT e.ht_nbr, e.emp_nbr, m.emp_lname, m.emp_fname, e.end_last_annual_review AS REVIEW_DATE
+SELECT e.ht_nbr, e.emp_nbr, m.emp_lname, m.emp_fname, to_char(e.end_last_annual_review, 'Day DD MON YYYY' ) AS REVIEW_DATE
 FROM mh.endorsement e JOIN mh.employee m ON e.emp_nbr = m.emp_nbr
 WHERE e.end_last_annual_review > to_date('31/03/2020', 'DD/MM/YYYY')
 ORDER BY review_date ASC;
@@ -29,13 +29,23 @@ ORDER BY review_date ASC;
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
-
+SELECT c.charter_nbr, c.client_nbr, l.client_lname, l.client_fname, c.charter_special_reqs
+FROM mh.charter c JOIN mh.client l ON c.client_nbr = l.client_nbr
+WHERE c.charter_special_reqs IS NOT NULL
+ORDER BY c.charter_nbr ASC;
 
 /*
     Q3
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+SELECT c.charter_nbr, (e.client_lname || ' ' || e.client_fname) as FULLNAME, c.charter_cost_per_hour
+FROM mh.charter c JOIN mh.client e ON c.client_nbr = e.client_nbr
+WHERE (c.charter_cost_per_hour < 1000 OR c.charter_special_reqs IS NULL) AND c.charter_nbr IN
+    (SELECT a.charter_nbr
+    FROM mh.charter_leg a JOIN mh.location o ON a.location_nbr_destination = o.location_nbr
+    WHERE o.location_name = 'Mount Doom')
+ORDER BY FULLNAME DESC;
 
 
 /*
@@ -43,6 +53,10 @@ ORDER BY review_date ASC;
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+SELECT t.ht_nbr, t.ht_name, count(h.heli_callsign)
+FROM mh.helicopter_type t JOIN mh.helicopter h ON t.ht_nbr = h.ht_nbr
+GROUP BY t.ht_nbr, t.ht_name
+HAVING count(h.heli_callsign) > 1;
 
 
 /*
@@ -50,6 +64,11 @@ ORDER BY review_date ASC;
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+SELECT t.location_nbr, t.location_name, count(h.cl_leg_nbr) AS TIMES_USED
+FROM mh.location t JOIN mh.charter_leg h ON t.location_nbr = h.location_nbr_origin
+GROUP BY t.location_nbr, t.location_name
+HAVING count(h.cl_leg_nbr) > 1
+ORDER BY TIMES_USED ASC;
 
 
 /*
