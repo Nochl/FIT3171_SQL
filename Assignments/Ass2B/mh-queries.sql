b83578f255ed56a5b54478602662a8a8272b5513
eleo0010 <eleo0010@MVAZ2STUL01049.ad.monash.edu>
2021-05-26 13:46:36 +1000

    Completed Task 1 Q1

--- Assignments/Ass2B/mh-queries.sql
+++ Assignments/Ass2B/mh-queries.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --mh-queries.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 30600022
+--Student Name: Enoch Leow
+--Tutorial No: 06_LiveStream
 
 /* Comments for your marker:
 
@@ -18,6 +18,10 @@
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+SELECT e.ht_nbr, e.emp_nbr, m.emp_lname, m.emp_fname, e.end_last_annual_review AS REVIEW_DATE
+FROM mh.endorsement e JOIN mh.employee m ON e.emp_nbr = m.emp_nbr
+WHERE e.end_last_annual_review > to_date('31/03/2020', 'DD/MM/YYYY')
+ORDER BY review_date ASC;
 
 
 /*
