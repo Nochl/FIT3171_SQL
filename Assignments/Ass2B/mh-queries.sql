4506c55d34d07f2b84808041c344b2f64a253972
eleo0010 <eleo0010@MVAZ1STUL01093.ad.monash.edu>
2021-05-28 00:48:42 +1000

    Completed Task 1 Q6-8

--- Assignments/Ass2B/mh-queries.sql
+++ Assignments/Ass2B/mh-queries.sql
@@ -76,13 +76,29 @@ ORDER BY TIMES_USED ASC;
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
-
+SELECT h.heli_callsign as CALLSIGN, t.ht_name as HELICOPTER_TYPE, NVL(h.heli_hrs_flown,  0) as HOURS_FLOWN
+FROM mh.helicopter h RIGHT OUTER JOIN mh.helicopter_type t ON h.ht_nbr = t.ht_nbr
+ORDER BY HOURS_FLOWN ASC;
 
 /*
     Q7
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+SELECT c.charter_nbr, to_char(l.cl_etd, 'DD-MON-RR HH24:MI') AS Date_Time_of_Departure
+FROM mh.charter c JOIN mh.charter_leg l ON c.charter_nbr = l.charter_nbr
+WHERE c.charter_nbr IN
+    (SELECT c.charter_nbr
+    FROM (mh.charter c JOIN mh.employee e ON c.emp_nbr = e.emp_nbr) 
+        JOIN mh.charter_leg l
+        ON c.charter_nbr = l.charter_nbr
+    WHERE c.charter_nbr IN (SELECT c.charter_nbr 
+                            FROM mh.charter c JOIN mh.employee e ON c.emp_nbr = e.emp_nbr
+                            WHERE e.emp_fname = 'Frodo' AND e.emp_lname = 'Baggins')                 
+    GROUP BY c.charter_nbr
+    HAVING count(l.cl_leg_nbr) = count (l.cl_ata))
+AND l.cl_leg_nbr = 1
+ORDER BY Date_Time_of_Departure DESC;
 
 
 /*
@@ -90,7 +106,23 @@ ORDER BY TIMES_USED ASC;
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
-
+SELECT c.charter_nbr, c.client_nbr, nvl(t.client_lname, '-') as client_lname, 
+       nvl(t.client_fname, '-') as client_fname, 
+       ('$' || to_char(calculated.totalchartercost, '999.99')) AS TOTALCHARTERCOST
+FROM ((SELECT c.charter_nbr,c.charter_cost_per_hour * sum(24*(l.cl_ata-l.cl_atd))AS TOTALCHARTERCOST
+        FROM (mh.charter c JOIN mh.employee e ON c.emp_nbr = e.emp_nbr) 
+        JOIN mh.charter_leg l
+        ON c.charter_nbr = l.charter_nbr               
+        GROUP BY c.charter_nbr, c.charter_cost_per_hour
+        HAVING count(l.cl_leg_nbr) = count (l.cl_ata) AND c.charter_cost_per_hour * sum(24*(l.cl_ata-l.cl_atd)) < 
+            (SELECT avg(Charter_cost) as AVERAGE_CHARTER_COST
+            FROM (SELECT l.charter_nbr, c.charter_cost_per_hour*(sum(24*(l.cl_ata-l.cl_atd))) as Charter_cost
+            FROM mh.charter_leg l JOIN mh.charter c ON l.charter_nbr = c.charter_nbr
+            GROUP BY l.charter_nbr, c.charter_cost_per_hour
+            HAVING sum(24*(l.cl_ata-l.cl_atd)) IS NOT NULL)cost))calculated  
+        JOIN mh.charter c ON calculated.charter_nbr = c.charter_nbr)
+        JOIN mh.client t ON c.client_nbr = t.client_nbr
+ORDER BY TOTALCHARTERCOST DESC;
 
 /*
     Q9
